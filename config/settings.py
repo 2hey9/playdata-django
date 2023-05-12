96de2cc15b0c0a421fc05de4a1254ddbce38f259
2hey9 <heagyu2001@naver.com>
2023-05-12 12:44:08 +0900

    db setting modify

--- config/settings.py
+++ config/settings.py
@@ -77,9 +77,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pybo',
+        'USER' : 'root',
+        'PASSWORD' : '1234',
+        'HOST' : '54.180.134.205',
+        'PORT' : '3306'
     }
+
 }
 
 
