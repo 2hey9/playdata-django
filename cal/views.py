1b4ea4f9989651021df815fb0ebbd6cfa9daafe7
2hey9 <heagyu2001@naver.com>
2023-05-15 03:32:24 +0900

    cal 2nd update

--- cal/views.py
+++ cal/views.py
@@ -1,8 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timedelta, date
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from django.utils.safestring import mark_safe
+import calendar
 
 from .models import *
 from .utils import Calendar
@@ -25,11 +26,32 @@ class CalendarView(generic.ListView):
         # Call the formatmonth method, which returns our calendar as a table
         html_cal = cal.formatmonth(withyear=True)
         context['calendar'] = mark_safe(html_cal)
+
+        # Get previous and next month
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
+
         return context
     
 def get_date(req_day):
-    if req_day: 
-        year, month = (int(x) for x in req_day.split('-'))
-        return date(year, month, day=1)
-    return datetime.today()
+    try:
+        if req_day: 
+            year, month, day = (int(x) for x in req_day.split('-'))
+            return date(year, month, day=1)
+    except (ValueError, TypeError):
+        pass
+    return datetime.today().date()
 
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    a = 'day=' + str(prev_month.year) + '-' + str(prev_month.month) + '-' + str(prev_month.day)
+    return a
+
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    a = 'day=' + str(next_month.year) + '-' + str(next_month.month) + '-' + str(next_month.day)
+    return a
