bc2f7481251c0b64b9e9c1c85d73bcd706978f31
SBBetter <alskdjqpw@gmail.com>
2023-05-16 14:10:58 +0900

    requirements.txt 추가

--- cal/urls.py
+++ cal/urls.py
@@ -3,8 +3,8 @@ from cal import views
 
 app_name = 'cal'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('calendar/', views.CalendarView.as_view(), name='calendar'),
+    path('calendar/', views.index, name='index'),
+    path('', views.CalendarView.as_view(), name='calendar'),
     path('event/new/', views.event, name='event_new'),
     path('event/edit/<int:event_id>/', views.event, name='event_edit'),
 ]
