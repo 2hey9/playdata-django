94102a1dc26be043953e24c4cd177af3920935a1
2hey9 <heagyu2001@naver.com>
2023-05-18 09:32:35 +0900

    캘린더 디자인 1차 수정

--- cal/utils.py
+++ cal/utils.py
@@ -17,7 +17,7 @@ class Calendar(HTMLCalendar):
 			d += f'<li> {event.get_html_url} </li>'
 
 		if day != 0:
-			return f"<td><span class='date'>{day}</span><ul> {d} </ul></td>"
+			return f"<td><span class='date'>{day}</span><ul>{d}</ul></td>"
 		return '<td></td>'
 
 	# formats a week as a tr
@@ -32,7 +32,7 @@ class Calendar(HTMLCalendar):
 	def formatmonth(self, withyear=True):
 		events = Event.objects.filter(start_time__year=self.year, start_time__month=self.month)
 
-		cal = f'<table border="0" cellpadding="0" cellspacing="0" class="calendar">\n'
+		cal = f'<table cellspacing="0" class="calendar">\n'
 		cal += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}\n'
 		cal += f'{self.formatweekheader()}\n'
 		for week in self.monthdays2calendar(self.year, self.month):
