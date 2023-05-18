fa8f21c1a9b2403019b936bd2acd153da326d23f
2hey9 <heagyu2001@naver.com>
2023-05-18 22:03:18 +0900

    local에서 실행

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'ls&1oied3t*np!^y6664m!j2-l%ie=8g$w-bjk_pjryzd#3y6b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["54.180.134.205"]
+# ALLOWED_HOSTS = ["54.180.134.205"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -82,14 +83,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_db', 
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST': '54.180.134.205',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'django_db', 
+    #     'USER': 'root',
+    #     'PASSWORD': '1234',
+    #     'HOST': '54.180.134.205',
+    #     'PORT': '3306',
+    # }
+    }
 
 
 # Password validation
