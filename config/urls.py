7b6cbf1e72421c0627645b95c9d67b2b4bfb2465
Lucky-Bastard0527 <invictus6578@gmail.com>
2023-05-18 11:43:48 +0900

    실시간 채팅창 왼쪽에 구현 에러 수정

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
 from pybo.views import base_views
-from chat import views as chat
 
 urlpatterns = [
     path('admin/', admin.site.urls),
